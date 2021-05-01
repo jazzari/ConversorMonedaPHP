d4693ed91a874280a14071c10dafc2cbf5e90904
jazzari <passionjazz@gmail.com>
2021-05-01 11:09:10 +0200

    Conversor de moneda

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<?php  
+
+    if (isset($_POST['monto'])) {
+      define ("cantidad", $_POST['monto']);
+    } else {
+      define ("cantidad", 0);
+    }
+    if (isset($_POST['moneda'])) {
+        define ("mon", $_POST['moneda']);
+    }
+    $peseta = 166.386;
+    $dolar = 1.21;
+    $conversion = 0;
+
+    if (mon == "pesetas") {
+        $conversion=cantidad*$peseta;
+        echo cantidad, " euros son ", number_format($conversion,3,",","."), " pesetas.";
+    }
+    else {
+        $conversion=cantidad*$dolar;
+        echo cantidad, " euros son ", number_format($conversion,3,",","."), " dólares.";
+    } 
+?>
+   
