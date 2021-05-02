e7a693d61d3525bad222fc8dd1f80c74b50055cf
jazzari <passionjazz@gmail.com>
2021-05-02 17:01:34 +0200

    Fix variable

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
       define ("cantidad", 0);
     }
     if (isset($_POST['moneda'])) {
-        define ("mon", $_POST['moneda']);
+       // define ("mon", $_POST['moneda']);
+        $mon = $_POST['moneda'];
     }
     $peseta = 166.386;
     $dolar = 1.21;
     $conversion = 0;
 
-    if (mon == "pesetas") {
+    if ($mon == "pesetas") {
         $conversion=cantidad*$peseta;
         echo cantidad, " euros son ", number_format($conversion,3,",","."), " pesetas.";
     }
